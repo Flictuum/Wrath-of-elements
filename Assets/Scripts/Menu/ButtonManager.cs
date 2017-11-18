2ddea34f42280be6f9a5325a9c46989d80e21106
Florent Bertrand <florent.5777@gmail.com>
2017-11-18 18:39:20 -0500

    Add functional script for button and sound in MainMenu

---
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void CreateGame(string newSceneName)
+	{
+		SceneManager.LoadScene(newSceneName);
+	}
+
+	public void JoinGame(string newSceneName)
+	{
+		SceneManager.LoadScene(newSceneName);
+	}
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
