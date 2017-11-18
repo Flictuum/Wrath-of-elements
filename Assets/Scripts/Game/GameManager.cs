b436e532a80acdc99b35c64994a4e40cfd222733
Rémi Weislinger <closingin@protonmail.com>
2017-11-18 17:23:21 -0500

    Organized the game scene, added the map generation

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject MapPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+		CreateMap ();
+	}
+
+	void CreateMap () {
+		GameObject Map = Instantiate (MapPrefab);
+		Map.name = "Map";
+		Map.transform.parent = this.transform;
+
+		MapGenerator Generator;
+		Generator = Map.transform.GetComponent<MapGenerator> ();
+		Generator.Create (Random.Range(0, 100), 30, 30);
 	}
 	
 	// Update is called once per frame
