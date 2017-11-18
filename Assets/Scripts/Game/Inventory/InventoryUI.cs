7f8b6ec33340ee16e760281c4fadaddc84e8a126
burg_c <burg_c@git.epitech.eu>
2017-11-18 18:48:50 -0500

    add inventory in game scene

---
+++ Assets/Scripts/Game/Inventory/InventoryUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    Inventory inventory;
+    InventorySlot[] slots;
+
+	// Use this for initialization
+	void Start () {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+        inventory.onItemChangedCallback.Invoke();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Inventory"))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+	}
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].addItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].clearSlot();
+            }
+        }
+    }
+}
